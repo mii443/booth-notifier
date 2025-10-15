23ee17498b29b2c7606359a36acdfb7953360f35
mii443 <mii@sfc.wide.ad.jp>
2025-10-15 13:30:43 +0900

    init

---
+++ src/event_handler.rs
@@ -0,0 +1,12 @@
+use poise::serenity_prelude::{self as serenity};
+
+use crate::{Data, Error};
+
+pub async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, Data, Error>,
+    data: &Data,
+) -> Result<(), Error> {
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+mod event_handler;
+
+use anyhow::Result;
+use event_handler::event_handler;
+use poise::serenity_prelude::{self as serenity};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub struct Data;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let token = std::env::var("BOT_TOKEN")?;
+    let owners = std::env::var("BOT_OWNERS")?
+        .split(',')
+        .map(|s| s.parse().unwrap())
+        .collect::<std::collections::HashSet<_>>();
+
+    let framework = poise::Framework::builder()
+        .setup(move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data) }))
+        .options(poise::FrameworkOptions {
+            event_handler: |ctx, event, framework, data| {
+                Box::pin(event_handler(ctx, event, framework, data))
+            },
+            owners,
+            ..Default::default()
+        })
+        .build();
+
+    let mut client =
+        serenity::ClientBuilder::new(token, serenity::GatewayIntents::non_privileged())
+            .framework(framework)
+            .await?;
+
+    client.start().await?;
+
+    Ok(())
+}
